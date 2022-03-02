07c6618dee747b31af50bad20035684a090298c1
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2022-03-02 23:12:22 +0200

    fix seeder main

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,23 +13,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-//        $this->call(ScreenSeeder::class);
-//        $this->call(SliderSeeder::class);
-//        $this->call(PackageSeeder::class);
-//        $this->call(OfferSeeder::class);
-//        $this->call(PackageTypeSeeder::class);
-//        $this->call(MealTypeSeeder::class);
-//        $this->call(MealSeeder::class);
-//        $this->call(PageSeeder::class);
-          $this->call(SettingSeeder::class);
-//        $this->call(CouponSeeder::class);
-//        $this->call(DemoAccountsSeeder::class);
-//        $this->call(PackageTypePriceSeeder::class);
-//        $this->call(PackageMealTypeSeeder::class);
-//        $this->call(PackageMealSeeder::class);
-//        $this->call(LocationSeeder::class);
-//        $this->call(NotificationSeeder::class);
+        $this->call(ScreenSeeder::class);
+        $this->call(SliderSeeder::class);
+        $this->call(PackageSeeder::class);
+        $this->call(OfferSeeder::class);
+        $this->call(PackageTypeSeeder::class);
+        $this->call(MealTypeSeeder::class);
+        $this->call(MealSeeder::class);
+        $this->call(PageSeeder::class);
+        $this->call(SettingSeeder::class);
+        $this->call(CouponSeeder::class);
+        $this->call(DemoAccountsSeeder::class);
+        $this->call(PackageTypePriceSeeder::class);
+        $this->call(PackageMealTypeSeeder::class);
+        $this->call(PackageMealSeeder::class);
+        $this->call(LocationSeeder::class);
+        $this->call(NotificationSeeder::class);
     }
 }
