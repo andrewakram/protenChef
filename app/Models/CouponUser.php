bc7101ae63b2d835bcd6a8652ae8c52cfab44a3b
MostafaElebzary <mostafaelebzary@gmail.com>
2022-03-02 21:41:24 +0200

    m

---
+++ app/Models/CouponUser.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CouponUser extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'coupon_id',
+        'used',
+
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class,'user_id');
+    }
+    public function coupon(){
+        return $this->belongsTo(Coupon::class,'coupon_id');
+    }
+}
