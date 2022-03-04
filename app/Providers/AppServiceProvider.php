689d14373512124d971723ab0fcff1febfaea72b
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2022-03-04 02:23:19 +0200

    fix Schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
