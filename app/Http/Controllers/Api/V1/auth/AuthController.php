f4464cdaba205b51c28facf3789333bb0bda67d4
MostafaElebzary <mostafaelebzary@gmail.com>
2022-03-08 22:18:58 +0200

    M

--- app/Http/Controllers/Api/V1/auth/AuthController.php
+++ app/Http/Controllers/Api/V1/auth/AuthController.php
@@ -161,11 +161,11 @@ class AuthController extends Controller
 
         $user = User::where('phone', $request->phone)->first();
 
-        $type = $user->active == 0 ? "activate" : "reset";
+//        $type = $user->active == 0 ? "activate" : "reset";
 
         $verfication = Verfication::where('phone', $request->phone)
             ->where('code', $request->code)
-            ->where('type', $type)
+//            ->where('type', $type)
             ->first();
 
 
@@ -173,17 +173,17 @@ class AuthController extends Controller
             if (!$verfication->expired_at > Carbon::now()->toDateTimeString()) {
                 return response()->json(msg($request, failed(), trans('lang.codeExpired')));
             }
-            if ($type == "activate") {
+//            if ($type == "activate") {
                 $user->active = 1;
                 $user->save();
                 $jwt_token = JWTAuth::fromUser($user);
                 $data = (new UsersResources($user))->token($jwt_token);
                 return response()->json(msgdata($request, success(), trans('lang.Verified_success'), $data));
-            } else {
-                $jwt_token = JWTAuth::fromUser($user);
-                $data = (new UsersResources($user))->token($jwt_token);
-                return response()->json(msgdata($request, success(), trans('lang.Verified_success'), $data));
-            }
+//            } else {
+//                $jwt_token = JWTAuth::fromUser($user);
+//                $data = (new UsersResources($user))->token($jwt_token);
+//                return response()->json(msgdata($request, success(), trans('lang.Verified_success'), $data));
+//            }
         } else {
             return response()->json(msg($request, failed(), trans('lang.codeError')));
         }
