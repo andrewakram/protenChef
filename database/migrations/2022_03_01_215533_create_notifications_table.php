d7a9375eb1caf5237b813021aceedb16747a080d
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2022-04-09 17:34:03 +0200

    refix notificaion seeder and database

--- database/migrations/2022_03_01_215533_create_notifications_table.php
+++ database/migrations/2022_03_01_215533_create_notifications_table.php
@@ -15,12 +15,13 @@ class CreateNotificationsTable extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('body');
+            $table->string('title_ar');
+            $table->string('title_en');
+            $table->string('body_ar');
+            $table->string('body_en');
             $table->bigInteger('model_id')->nullable();
             $table->enum('model_type', ['other','Coupon','Order','Meal','Offer'])->default('other');
             $table->foreignId('user_id')->nullable()->references('id')->on('users')->onDelete('set null');
-
             $table->timestamps();
         });
     }
