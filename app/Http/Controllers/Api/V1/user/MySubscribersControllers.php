a32fc0caa37321fc5a21c4297744688b958498a6
MostafaElebzary <mostafaelebzary@gmail.com>
2022-04-17 22:57:34 +0200

    M

--- app/Http/Controllers/Api/V1/user/MySubscribersControllers.php
+++ app/Http/Controllers/Api/V1/user/MySubscribersControllers.php
@@ -225,8 +225,7 @@ class MySubscribersControllers extends Controller
 //        TODO
         //sort array
 
-        dd(asort($old_dates));
-        $dates = collect($old_dates)->sortBy('Value')->values();
+        $dates = collect(asort($old_dates))->values();
 
 
 
