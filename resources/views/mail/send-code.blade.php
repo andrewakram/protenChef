8ceb6f60c324069e331d03f7003c109f773857ee
MostafaElebzary <mostafaelebzary@gmail.com>
2022-05-28 15:34:53 +0200

    M

--- resources/views/mail/send-code.blade.php
+++ resources/views/mail/send-code.blade.php
@@ -1,12 +1,28 @@
-@component('mail::message')
-       تأكيد
+{{--@component('mail::message')--}}
+{{--    تأكيد--}}
 
-    رمز التحقق الخاص بك هو :
-    {{ $token }}
+{{--    رمز التحقق الخاص بك هو :--}}
+{{--    {{ $token }}--}}
 
-    , شكرآ
+{{--    , شكرآ--}}
 
 
-    protein-chef
+{{--    protein-chef--}}
 
-@endcomponent
+{{--    @endcomponent--}}
+
+    <!doctype html>
+    <html>
+    <body>
+    <h1 style="color: #Fd5A1F">Protien Chef</h1>
+    <h3>
+        <span> كود التفعيل هو </span>
+        {{--    <span> Allo Chef </span>--}}
+    </h3>
+    <br>
+
+    <a href="" style="background-color: #Fd5A1F; color: #fff; padding: 20px">
+        {{ $token }}
+    </a>
+    </body>
+    </html>
