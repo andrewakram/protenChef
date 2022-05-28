e8db5cdf577969a954bb91ed68a13e3b2dba3318
MostafaElebzary <mostafaelebzary@gmail.com>
2022-05-28 16:09:04 +0200

    M

--- resources/views/mail/send-code.blade.php
+++ resources/views/mail/send-code.blade.php
@@ -17,14 +17,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <title>{{__("Reset Password")}}</title>
+    <title> التحقق </title>
 </head>
 <body style="background-color: #f5f5f5; margin: 0; padding: 30px 0;">
 <table align="center" width="600" callspacing="0" cellpadding="0" style="margin-top: 0; max-width: 600px; width: 100%;border-spacing: 0;">
     <tbody>
     <tr>
         <td style="padding: 30px 30px 0; text-align: center;">
-            <img width="220px" height="120px" src="{{ @url('website') }}/img/logo.png" alt="{{config('app.name')}}">
+            <img width="220px" height="120px" src="{{ @url('/') }}/default.png" alt="{{config('app.name')}}">
         </td>
     </tr>
     </tbody>
@@ -36,10 +36,10 @@
             <table width="600" callspacing="0" cellpadding="0" style="margin-top: 30px; width: 600px; max-width: 600px; border-spacing: 0;">
                 <thead>
                 <tr>
-                    <th align="center" colspan="2" class="top-border" style="margin:0; padding:0; background-color: #00aeef; border-radius: 30px 30px 0 0; text-align: center;" valign="top">
-                        <img src="{{@asset('assets/email/images/round-top-blue.png')}}" alt="" width="100%">
+                    <th align="center" colspan="2" class="top-border" style="margin:0; padding:0; background-color: #f7941d; border-radius: 30px 30px 0 0; text-align: center;" valign="top">
+{{--                        <img src="{{@asset('assets/email/images/round-top-blue.png')}}" alt="" width="100%">--}}
                         <h3 class="text-large" style="color: #fff; font-family: Arial; font-size: 24px; margin-bottom: 20px; margin-top: 0;">
-                            <b>{{__("Reset Password")}}</b>
+                            <b>التحقق</b>
                         </h3>
                     </th>
                 </tr>
@@ -48,18 +48,18 @@
                 <tbody>
                 <tr>
                     <td class="bottom-border" style="background-color: #fff;padding: 30px 30px 0;">
-                        <p>You are receiving this email because we received a password reset request for your account.</p>
+                        <p>أنت تتلقى هذا البريد الإلكتروني لأننا تلقينا طلب بالتحقق من حسابك.</p>
                         <table align="center" cellpadding="0" cellspacing="0" style="border-spacing:0; height:37px; margin-bottom:30px">
                             <tbody>
                             <tr style="background-color: #f7941d;">
-                                <td style="background-color:#ffffff; border-spacing:0; border-width:0px; padding:0; text-align:right; vertical-align: top;"><img alt=" " height="100%" src="https://hand2hand.magic-solution.com/assets/email/images/round-left.png" /></td>
-                                <td style="background-color:#f7941d; border-width:0px; padding:0"> Your Verification Code is  {{ $token }} </td>
-                                <td style="background-color:#ffffff; border-spacing:0; border-width:0px; padding:0; text-align:left; vertical-align: top;"><img alt=" " height="100%" src="https://hand2hand.magic-solution.com/assets/email/images/round-right.png" /></td>
+{{--                                <td style="background-color:#ffffff; border-spacing:0; border-width:0px; padding:0; text-align:right; vertical-align: top;"><img alt=" " height="100%" src="https://hand2hand.magic-solution.com/assets/email/images/round-left.png" /></td>--}}
+                                <td style="color:#f7941d; border-width:0px; padding:0"> رمز التحقق الخاص بك هو  {{ $token }} </td>
+{{--                                <td style="background-color:#ffffff; border-spacing:0; border-width:0px; padding:0; text-align:left; vertical-align: top;"><img alt=" " height="100%" src="https://hand2hand.magic-solution.com/assets/email/images/round-right.png" /></td>--}}
                             </tr>
                             </tbody>
                         </table>
-                        <p class="small">This code is available for one use ,and will be expired in an hour from now</p>
-                        <p class="small">If you did not request a password reset, no further action is required</p>
+                        <p class="small">هذا الرمز متاح للاستخدام مرة واحدة وستنتهي صلاحيته بعد ساعة من الآن</p>
+                        <p class="small">إذا لم تطلب التحقق من حسابك ، فلا داعي لاتخاذ أي إجراء آخر</p>
                     </td>
                 </tr>
                 <tr>
