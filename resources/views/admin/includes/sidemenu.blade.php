c0fa49af314aea195163a5ee4c5fff2d8a74b0bd
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2022-06-01 23:44:47 +0200

    hide menue item

--- resources/views/admin/includes/sidemenu.blade.php
+++ resources/views/admin/includes/sidemenu.blade.php
@@ -779,52 +779,52 @@
                         <span class="menu-title">إعدادات الإشعارات</span>
                     </a>
                 </div>
-                <div data-kt-menu-trigger="click"
-                     class="menu-item menu-accordion @if(request()->segment(2) == 'package_types_settings') hover show  @endif">
-                    <span class="menu-link @if(request()->segment(2) == 'package_types_settings') active @endif">
-                        <span class="menu-icon">
-                            <!--begin::Svg Icon | path: icons/duotune/ecommerce/ecm007.svg-->
-                            <span class="svg-icon svg-icon-primary svg-icon-2x"><!--begin::Svg Icon | path:C:\wamp64\www\keenthemes\themes\metronic\theme\html\demo1\dist/../src/media/svg/icons\Shopping\Money.svg--><svg
-                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                                    width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <rect x="0" y="0" width="24" height="24"/>
-        <path
-            d="M2,6 L21,6 C21.5522847,6 22,6.44771525 22,7 L22,17 C22,17.5522847 21.5522847,18 21,18 L2,18 C1.44771525,18 1,17.5522847 1,17 L1,7 C1,6.44771525 1.44771525,6 2,6 Z M11.5,16 C13.709139,16 15.5,14.209139 15.5,12 C15.5,9.790861 13.709139,8 11.5,8 C9.290861,8 7.5,9.790861 7.5,12 C7.5,14.209139 9.290861,16 11.5,16 Z"
-            fill="#000000" opacity="0.3"
-            transform="translate(11.500000, 12.000000) rotate(-345.000000) translate(-11.500000, -12.000000) "/>
-        <path
-            d="M2,6 L21,6 C21.5522847,6 22,6.44771525 22,7 L22,17 C22,17.5522847 21.5522847,18 21,18 L2,18 C1.44771525,18 1,17.5522847 1,17 L1,7 C1,6.44771525 1.44771525,6 2,6 Z M11.5,16 C13.709139,16 15.5,14.209139 15.5,12 C15.5,9.790861 13.709139,8 11.5,8 C9.290861,8 7.5,9.790861 7.5,12 C7.5,14.209139 9.290861,16 11.5,16 Z M11.5,14 C12.6045695,14 13.5,13.1045695 13.5,12 C13.5,10.8954305 12.6045695,10 11.5,10 C10.3954305,10 9.5,10.8954305 9.5,12 C9.5,13.1045695 10.3954305,14 11.5,14 Z"
-            fill="#000000"/>
-    </g>
-</svg><!--end::Svg Icon--></span>
-                            <!--end::Svg Icon-->
-                        </span>
-                        <span class="menu-title">إعدادات انواع الباقات</span>
-                        <span class="menu-arrow"></span>
-                    </span>
-                    <div class="menu-sub menu-sub-accordion menu-active-bg">
-                        <div class="menu-item">
-                            <a class="menu-link @if(request()->segment(2) == 'package_types_settings' && request()->segment(3) == 'dynamic_types') active @endif"
-                               href="{{route('admin.package_types_settings.dynamic_types')}}">
-												<span class="menu-bullet">
-													<span class="bullet bullet-dot"></span>
-												</span>
-                                <span class="menu-title">انواع الايام</span>
-                            </a>
-                        </div>
-                        <div class="menu-item">
-                            <a class="menu-link @if(request()->segment(2) == 'package_types_settings' && request()->segment(3) == 'dynamic_times') active @endif"
-                               href="{{route('admin.package_types_settings.dynamic_times')}}">
-												<span class="menu-bullet">
-													<span class="bullet bullet-dot"></span>
-												</span>
-                                <span class="menu-title">انواع التوقيت</span>
-                            </a>
-                        </div>
+{{--                <div data-kt-menu-trigger="click"--}}
+{{--                     class="menu-item menu-accordion @if(request()->segment(2) == 'package_types_settings') hover show  @endif">--}}
+{{--                    <span class="menu-link @if(request()->segment(2) == 'package_types_settings') active @endif">--}}
+{{--                        <span class="menu-icon">--}}
+{{--                            <!--begin::Svg Icon | path: icons/duotune/ecommerce/ecm007.svg-->--}}
+{{--                            <span class="svg-icon svg-icon-primary svg-icon-2x"><!--begin::Svg Icon | path:C:\wamp64\www\keenthemes\themes\metronic\theme\html\demo1\dist/../src/media/svg/icons\Shopping\Money.svg--><svg--}}
+{{--                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"--}}
+{{--                                    width="24px" height="24px" viewBox="0 0 24 24" version="1.1">--}}
+{{--    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">--}}
+{{--        <rect x="0" y="0" width="24" height="24"/>--}}
+{{--        <path--}}
+{{--            d="M2,6 L21,6 C21.5522847,6 22,6.44771525 22,7 L22,17 C22,17.5522847 21.5522847,18 21,18 L2,18 C1.44771525,18 1,17.5522847 1,17 L1,7 C1,6.44771525 1.44771525,6 2,6 Z M11.5,16 C13.709139,16 15.5,14.209139 15.5,12 C15.5,9.790861 13.709139,8 11.5,8 C9.290861,8 7.5,9.790861 7.5,12 C7.5,14.209139 9.290861,16 11.5,16 Z"--}}
+{{--            fill="#000000" opacity="0.3"--}}
+{{--            transform="translate(11.500000, 12.000000) rotate(-345.000000) translate(-11.500000, -12.000000) "/>--}}
+{{--        <path--}}
+{{--            d="M2,6 L21,6 C21.5522847,6 22,6.44771525 22,7 L22,17 C22,17.5522847 21.5522847,18 21,18 L2,18 C1.44771525,18 1,17.5522847 1,17 L1,7 C1,6.44771525 1.44771525,6 2,6 Z M11.5,16 C13.709139,16 15.5,14.209139 15.5,12 C15.5,9.790861 13.709139,8 11.5,8 C9.290861,8 7.5,9.790861 7.5,12 C7.5,14.209139 9.290861,16 11.5,16 Z M11.5,14 C12.6045695,14 13.5,13.1045695 13.5,12 C13.5,10.8954305 12.6045695,10 11.5,10 C10.3954305,10 9.5,10.8954305 9.5,12 C9.5,13.1045695 10.3954305,14 11.5,14 Z"--}}
+{{--            fill="#000000"/>--}}
+{{--    </g>--}}
+{{--</svg><!--end::Svg Icon--></span>--}}
+{{--                            <!--end::Svg Icon-->--}}
+{{--                        </span>--}}
+{{--                        <span class="menu-title">إعدادات انواع الباقات</span>--}}
+{{--                        <span class="menu-arrow"></span>--}}
+{{--                    </span>--}}
+{{--                    <div class="menu-sub menu-sub-accordion menu-active-bg">--}}
+{{--                        <div class="menu-item">--}}
+{{--                            <a class="menu-link @if(request()->segment(2) == 'package_types_settings' && request()->segment(3) == 'dynamic_types') active @endif"--}}
+{{--                               href="{{route('admin.package_types_settings.dynamic_types')}}">--}}
+{{--												<span class="menu-bullet">--}}
+{{--													<span class="bullet bullet-dot"></span>--}}
+{{--												</span>--}}
+{{--                                <span class="menu-title">انواع الايام</span>--}}
+{{--                            </a>--}}
+{{--                        </div>--}}
+{{--                        <div class="menu-item">--}}
+{{--                            <a class="menu-link @if(request()->segment(2) == 'package_types_settings' && request()->segment(3) == 'dynamic_times') active @endif"--}}
+{{--                               href="{{route('admin.package_types_settings.dynamic_times')}}">--}}
+{{--												<span class="menu-bullet">--}}
+{{--													<span class="bullet bullet-dot"></span>--}}
+{{--												</span>--}}
+{{--                                <span class="menu-title">انواع التوقيت</span>--}}
+{{--                            </a>--}}
+{{--                        </div>--}}
 
-                    </div>
-                </div>
+{{--                    </div>--}}
+{{--                </div>--}}
             </div>
             <!--end::Menu-->
         </div>
