bb69cbfa4229395a137510a896daf252d30bd139
Andrew Akram Albert <andrewalbert93501@gmail.com>
2022-07-30 02:33:45 +0200

    make some apis accept guest mode

--- routes/api.php
+++ routes/api.php
@@ -61,6 +61,17 @@ Route::group(['prefix' => "V1", 'namespace' => 'V1'], function () {
         Route::get('/package_price_details/{package_pricec_id}', [PackagesController::class, 'package_price_details']);
         Route::get('/package_menu_meals', [PackagesController::class, 'package_menu_meals']);
         Route::get('/meal/details/{id}', [PackagesController::class, 'meal_details']);
+    });
+
+    Route::group(['prefix' => "user", 'middleware' => ['auth:api','check_active']], function () {
+        //home
+//        Route::get('/home', [HomeController::class, 'home']);
+//        Route::get('/package_parent_type', [PackagesController::class, 'package_parent_type']);
+//        Route::get('/package_types', [PackagesController::class, 'package_types']);
+//        Route::get('/package_meal_types/{type}', [PackagesController::class, 'package_meal_types']);
+//        Route::get('/package_price_details/{package_pricec_id}', [PackagesController::class, 'package_price_details']);
+//        Route::get('/package_menu_meals', [PackagesController::class, 'package_menu_meals']);
+//        Route::get('/meal/details/{id}', [PackagesController::class, 'meal_details']);
 
         //order
         Route::post('/make_order', [OrderController::class, 'make_order']);
